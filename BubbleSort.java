89e9c5570d203113697d00f1854b4fecdd534629
Shivam <96533413+ShivamChavan01@users.noreply.github.com>
2025-02-17 15:00:10 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -2,44 +2,32 @@ package com.kn.ArrayTasks;
 
 public class BubbleSort {
 
-	public static void main(Strings[] args){
-        int arr[] = {1,3,56,777,8,5}
-		int brr[]   = bubblesort(arr);
+    public static void main(String[] args) { // ❌ Fixed "Strings" to "String"
+        int arr[] = {1, 3, 56, 777, 8, 5}; // ❌ Missing semicolon
 
-		for(int i=0;i<arr.length;i++){
-		  System.out.println(arr[i]);
-		}
-		
-		
-		}
-	
-	public int [] bubbleSort(int arr[]) {
-		  
-		
-		for(int i=0;i<arr.length-1;i++) 
-		{
-			boolean isSwapped=false;
-			for(int j=0;j<arr.length-1-i;j++)
-			{
-				     if(arr[j]>arr[j+1]) 
-				       {
-					int swap=arr[j];
-					arr[j]=arr[j+1];
-					arr[j+1]=swap;
-				       }
-				isSwapped=true;
-			}
-			if(isSwapped==false) 
-			{
-				break;
-			}
-			
-		}
-		return arr;
-		 
-		
-		
-		
-	}
+        BubbleSort bs = new BubbleSort(); // ❌ Need an instance to call a non-static method
+        int sortedArr[] = bs.bubbleSort(arr);
 
+        for (int i = 0; i < sortedArr.length; i++) {
+            System.out.println(sortedArr[i]);
+        }
+    }
+
+    public int[] bubbleSort(int arr[]) {  // ❌ Changed method name from "bubblesort" to "bubbleSort"
+        for (int i = 0; i < arr.length - 1; i++) {
+            boolean isSwapped = false;
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int swap = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = swap;
+                    isSwapped = true; // ❌ This should be inside the if condition
+                }
+            }
+            if (!isSwapped) { // ✅ Optimized check
+                break;
+            }
+        }
+        return arr;
+    }
 }
