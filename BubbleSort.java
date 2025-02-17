de059f70c700c87dc0baeb2605d7423fae28411b
Shivam <96533413+ShivamChavan01@users.noreply.github.com>
2025-02-17 14:58:24 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,6 +1,17 @@
 package com.kn.ArrayTasks;
 
 public class BubbleSort {
+
+	public static void main(Strings[] args){
+        int arr[] = {1,3,56,777,8,5}
+		int brr[]   = bubblesort(arr);
+
+		for(int i=0;i<arr.length;i++){
+		  System.out.println(arr[i]);
+		}
+		
+		
+		}
 	
 	public int [] bubbleSort(int arr[]) {
 		  
